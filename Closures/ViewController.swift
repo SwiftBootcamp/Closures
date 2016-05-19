f6a846bf2fd7d2534391213df3d00fd0e942ef4f
giria <joan@giria.com>
2016-05-19 18:50:18 +0200

    first commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Joan Barrull Ribalta on 08/05/16.
+//  Copyright © 2016 com.giria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
